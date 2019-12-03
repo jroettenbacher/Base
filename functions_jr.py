c19b130eafdb96eda24d8c79c4f327f479da5146
Johannes <44004932+jroettenbacher@users.noreply.github.com>
2019-12-03 16:07:59 +0100

    created script with own functions

---
+++ functions_jr.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+# from https://stackoverflow.com/questions/893657/how-do-i-calculate-r-squared-using-python-and-numpy
+def polyfit(x, y, degree):
+    results = {}
+
+    coeffs = np.polyfit(x, y, degree)
+
+    # Polynomial Coefficients
+    results['polynomial'] = coeffs.tolist()
+
+    # r-squared
+    p = np.poly1d(coeffs)
+    # fit values, and mean
+    yhat = p(x)                         # or [p(z) for z in x]
+    ybar = np.sum(y)/len(y)          # or sum(y)/len(y)
+    ssreg = np.sum((yhat-ybar)**2)   # or sum([ (yihat - ybar)**2 for yihat in yhat])
+    sstot = np.sum((y - ybar)**2)    # or sum([ (yi - ybar)**2 for yi in y])
+    results['determination'] = ssreg / sstot
+
+    return results
+
+
+if __name__ == '__main__':
+    r2 = polyfit(np.linspace(0, 100), np.arange(50, 100), 1)
+    print(r2)
