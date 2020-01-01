899932ff34ea451a0693eb4ddb75de63ea9388c5
Johannes <44004932+jroettenbacher@users.noreply.github.com>
2020-01-01 15:24:50 +0100

    adjusted presentation plot style

--- dataset_statistics.py
+++ dataset_statistics.py
@@ -26,15 +26,10 @@ for year in range(2010, 2017):
 
 plot_df = pd.DataFrame({'Year': np.arange(2010, 2017), 'Count': counts_year})
 jr.set_presentation_plot_style()
-plt.style.use('ggplot')
-font = {'family': 'sans-serif', 'size': 24}
-figure = {'figsize': [16, 9], 'dpi': 300}
-plt.rc('font', **font)
-plt.rc('figure', **figure)
 plt.bar(plot_df['Year'], plot_df['Count'], color='blue')
 plt.xlabel("Year")
-plt.ylabel("Count")
-plt.title("Number of MIRA35 Doppler Spectra Files per Year")
+plt.ylabel("Days")
+plt.title("Number of Days with MIRA35 Cloud Radar Files")
 # plt.show()
 plt.savefig('files_per_year.png')
 plt.rcdefaults()
