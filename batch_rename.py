845a3a8a5f66d81e2fbdbbd78e4f90851e265328
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-04-02 17:07:51 +0200

    added check for second run on same directory

--- batch_rename.py
+++ batch_rename.py
@@ -8,12 +8,12 @@ import re
 path = "/projekt2/remsens/data/campaigns/eurec4a/RV-METEOR_DSHIP/upload_to_aeris/"
 os.chdir(path)
 print(os.getcwd())
-files = os.listdir(path)
+files = [f for f in os.listdir(path) if not f.startswith("RV")]
 check1 = input(f"Do you want to rename all those files:\n {files}")
 if check1.startswith("y"):
     print("Renaming all files...")
     for file in files:
         match = re.search(r"(?P<date>\d{8})", file)
         newname = f"RV-METEOR_DSHIP_1Hz_{match.group('date')}.dat"
-        os.rename(file, newname)
+        os.replace(file, newname)
         print(f"renamed {file} to {newname}")
