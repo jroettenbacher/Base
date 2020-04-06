d013a5261ec8189c4f9a6029eef4bdc1717ff53a
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-04-06 11:29:14 +0200

    changed dates

--- average_cloudnet_input.py
+++ average_cloudnet_input.py
@@ -32,8 +32,8 @@ if 'date' in kwargs:
     begin_dt = dt.datetime.strptime(date + ' 00:00:05', '%Y%m%d %H:%M:%S')
     end_dt = dt.datetime.strptime(date + ' 00:00:05', '%Y%m%d %H:%M:%S')
 else:
-    begin_dt = dt.datetime(2020, 2, 5, 0, 0, 0)
-    end_dt = dt.datetime(2020, 2, 6, 0, 0, 0)
+    begin_dt = dt.datetime(2020, 1, 17, 0, 0, 0)
+    end_dt = dt.datetime(2020, 1, 27, 0, 0, 0)
 
 dates = pd.date_range(begin_dt, end_dt).to_pydatetime()  # define all dates
 outpath = "/projekt2/remsens/data/campaigns/eurec4a/LIMRAD94/30s_averages/"  # define path for output
