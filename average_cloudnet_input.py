d6f66f0a8e64113f1ac303076f457b436b013888
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-04-06 11:25:28 +0200

    replaced masked values with -999

--- average_cloudnet_input.py
+++ average_cloudnet_input.py
@@ -50,10 +50,10 @@ for date in dates:
     # 30m range steps, started from rounded down to the next 100 first range gate, and rounded up to the next 1000 last rg
     new_range = np.arange(Ze['rg'][0]//100*100, np.round(Ze['rg'][-1], -3)+30, 30)  # 300-15000 or 300-13020 m
     Ze = interpolate2d(Ze, new_time=new_time, new_range=new_range, method='linear')
-    # mask minimal values because masked values get filled with minimal value during interpolation
-    Ze["var"] = np.ma.masked_where(Ze["var"] == np.nanmin(Ze["var"]), Ze["var"])
-    # overwrite mask in larda container -> does not change plot output
-    Ze["mask"] = Ze["var"].mask
+    # turn mask from 1s and 0s to True and False
+    Ze["mask"] = Ze["mask"] == 1
+    # fill masked values with -999
+    Ze["var"] = h.fill_with(Ze["var"], Ze["mask"], -999)
     # generate nc file
     container = {'Ze': Ze}  # create a container for the routine
     outfile = f"RV-METEOR_LIMRAD94_Ze_{date:%Y%m%d}.nc"
