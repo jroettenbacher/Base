874592ed1cfc522f5a8736f895c3f625413f3978
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-04-06 11:08:20 +0200

    added masking after averaging

--- average_cloudnet_input.py
+++ average_cloudnet_input.py
@@ -32,8 +32,8 @@ if 'date' in kwargs:
     begin_dt = dt.datetime.strptime(date + ' 00:00:05', '%Y%m%d %H:%M:%S')
     end_dt = dt.datetime.strptime(date + ' 00:00:05', '%Y%m%d %H:%M:%S')
 else:
-    begin_dt = dt.datetime(2020, 1, 17, 0, 0, 0)
-    end_dt = dt.datetime(2020, 2, 27, 0, 0, 0)
+    begin_dt = dt.datetime(2020, 2, 5, 0, 0, 0)
+    end_dt = dt.datetime(2020, 2, 6, 0, 0, 0)
 
 dates = pd.date_range(begin_dt, end_dt).to_pydatetime()  # define all dates
 outpath = "/projekt2/remsens/data/campaigns/eurec4a/LIMRAD94/upload_to_aeris/"  # define path for output
@@ -44,16 +44,16 @@ for date in dates:
     # load data from larda
     Ze = larda.read("LIMRAD94_cn_input", "Ze", [date, (date + dt.timedelta(hours=23, minutes=59, seconds=30))],
                     [0, 'max'])
-    # mask values = -999
-    Ze["var"] = np.ma.masked_where(Ze["var"] == -999, Ze["var"])
-    # overwrite mask in larda container -> does not change plot output
-    Ze["mask"] = Ze["var"].mask
     # define new dimensions
     new_time = np.array([date + dt.timedelta(seconds=i) for i in range(0, int((24*60*60)), 30)])  # 30s timestep
     new_time = np.array([h.dt_to_ts(time) for time in new_time])  # convert to unix time stamps
     # 30m range steps, started from rounded down to the next 100 first range gate, and rounded up to the next 1000 last rg
     new_range = np.arange(Ze['rg'][0]//100*100, np.round(Ze['rg'][-1], -3)+30, 30)  # 300-15000 or 300-13020 m
     Ze = interpolate2d(Ze, new_time=new_time, new_range=new_range, method='linear')
+    # mask minimal values because masked values get filled with minimal value during interpolation
+    Ze["var"] = np.ma.masked_where(Ze["var"] == np.nanmin(Ze["var"]), Ze["var"])
+    # overwrite mask in larda container -> does not change plot output
+    Ze["mask"] = Ze["var"].mask
     # generate nc file
     container = {'Ze': Ze}  # create a container for the routine
     outfile = f"RV-METEOR_LIMRAD94_Ze_{date:%Y%m%d}.nc"
