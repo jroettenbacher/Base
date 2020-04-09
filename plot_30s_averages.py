97d2e47c4889a77b15653686012a12e3f77882a4
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-04-09 18:31:06 +0200

    changed cbt plotting

--- plot_30s_averages.py
+++ plot_30s_averages.py
@@ -62,8 +62,9 @@ name = f'{plot_path}/{begin_dt:%Y%m%d_%H%M}_{end_dt:%Y%m%d_%H%M}_preliminary_{pl
 
 fig, ax = pyLARDA.Transformations.plot_timeheight(radar_Z, range_interval=plot_range, rg_converter=True, title=True,
                                                   z_converter='lin2z')
-ax.pcolormesh(matplotlib.dates.date2num(dt_list[:]), range_list[:], np.transpose(var[:, :]),
-              label='cloud bases and tops', cmap="PRGn")
+# ax.pcolormesh(matplotlib.dates.date2num(dt_list[:]), range_list[:], np.transpose(var[:, :]),
+#               label='cloud bases and tops', cmap="PRGn")
+ax.imshow(np.transpose(var), cmap="PRGn")
 ax.legend()
 fig.savefig(name + '_cbt_Z.png', dpi=250)
 print(f'figure saved :: {name}_cbt_Z.png')
