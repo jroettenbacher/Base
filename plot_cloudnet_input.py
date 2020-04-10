f9d7fafb082f6d5a28e63eafcb015bb3ecce1c83
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-04-10 09:48:24 +0200

    added cloud bases tops to cloudnet input plots

--- plot_cloudnet_input.py
+++ plot_cloudnet_input.py
@@ -7,7 +7,8 @@ sys.path.append('/projekt1/remsens/work/jroettenbacher/Base/larda')
 sys.path.append('.')
 
 import matplotlib
-
+from matplotlib.colors import ListedColormap
+from matplotlib.patches import Patch
 matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
@@ -22,6 +23,8 @@ log = logging.getLogger('pyLARDA')
 log.setLevel(logging.WARNING)
 log.addHandler(logging.StreamHandler())
 
+# define plot path
+plot_path = "/projekt1/remsens/work/jroettenbacher/plots"
 # Load LARDA
 larda = pyLARDA.LARDA().connect('eurec4a', build_lists=True)
 
@@ -62,9 +65,14 @@ radar_sw["mask"] = radar_Z["var"].mask
 # radar_Inc_El = larda.read(system, "Inc_El", [begin_dt, end_dt])
 # radar_Inc_ElA = larda.read(system, "Inc_ElA", [begin_dt, end_dt])
 # radar_LDR = larda.read(system, "ldr", [begin_dt, end_dt], plot_range)
+cloud_bases_tops = larda.read(system, "cloud_bases_tops", [begin_dt, end_dt], plot_range)
+dt_list = np.asarray([datetime.datetime.utcfromtimestamp(time) for time in cloud_bases_tops['ts']])
+range_list = cloud_bases_tops['rg']/1000.0
+var = cloud_bases_tops['var']
+# mask -999 = fill value
+var = np.ma.masked_where(var == -999, var)
 
-name = f'plots/' \
-       f'{begin_dt:%Y%m%d_%H%M}_{end_dt:%Y%m%d_%H%M}_preliminary_{plot_range[1] / 1000:.0f}km_cloudnet_input'
+name = f'{plot_path}/{begin_dt:%Y%m%d_%H%M}_{end_dt:%Y%m%d_%H%M}_preliminary_{plot_range[1] / 1000:.0f}km_cloudnet_input'
 
 # plot Roll and pitch
 # fig, ax = pyLARDA.Transformations.plot_timeseries(radar_Inc_El)
@@ -79,6 +87,18 @@ fig, _ = pyLARDA.Transformations.plot_timeheight(radar_Z, range_interval=plot_ra
                                                  z_converter='lin2z')
 fig.savefig(name + '_Z.png', dpi=250)
 print(f'figure saved :: {name}_Z.png')
+
+fig, ax = pyLARDA.Transformations.plot_timeheight(radar_Z, range_interval=plot_range, rg_converter=True, title=True,
+                                                  z_converter='lin2z')
+# create a binary color map for bases and tops
+cmap = ListedColormap(["darkgreen", "darkmagenta"])
+ax.pcolormesh(matplotlib.dates.date2num(dt_list[:]), range_list[:], np.transpose(var[:, :]),
+              label='cloud bases and tops', cmap=cmap)
+legend_elements = [Patch(facecolor='darkgreen', label='Bases'),
+                   Patch(facecolor='darkmagenta', label='Tops')]
+ax.legend(handles=legend_elements, title="Hydrometeor")
+fig.savefig(name + '_cbt_Z.png', dpi=250)
+print(f'figure saved :: {name}_cbt_Z.png')
 #
 # fig, _ = pyLARDA.Transformations.plot_timeheight(radar_MDV, rg_converter=True, title=True)
 # fig.savefig(name + '_MDV.png', dpi=250)
