8987a15cbad0187287ac4251b3796d11c726178f
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-04-14 14:39:05 +0200

    changed paths to radar

--- upload_to_aeris.sh
+++ upload_to_aeris.sh
@@ -14,9 +14,9 @@ HOST="ftp.climserv.ipsl.polytechnique.fr"  # define host server -> aeris
 RUSER="eurec4a"  # define user
 PASS="pass4eurec4a!"  # define password
 # define local directory
-#LCD="/projekt2/remsens/data/campaigns/eurec4a/LIMRAD94/upload_to_aeris"  # LIMRAD94 data (30s averages)
-LCD="/projekt2/remsens/data/campaigns/eurec4a/RV-METEOR_CEILOMETER/upload_to_aeris"  # ceilometer data (nc files)
+LCD="/projekt2/remsens/data/campaigns/eurec4a/LIMRAD94/upload_to_aeris"  # LIMRAD94 data (30s averages)
+#LCD="/projekt2/remsens/data/campaigns/eurec4a/RV-METEOR_CEILOMETER/upload_to_aeris"  # ceilometer data (nc files)
 #define remote directory
-#RCD="/upload/SHIPS/RV-METEOR/cloudradar/ncfiles"  # cloud radar
-RCD="/upload/SHIPS/RV-METEOR/ceilometer/ncfiles"  # ceilometer nc files
+RCD="/upload/SHIPS/RV-METEOR/cloudradar/ncfiles"  # cloud radar
+#RCD="/upload/SHIPS/RV-METEOR/ceilometer/ncfiles"  # ceilometer nc files
 lftp -e "mirror -R $LCD $RCD " -u "$RUSER","$PASS" "$HOST"
