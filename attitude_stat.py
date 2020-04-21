eeceaa2f5fe0e6ea97db602643d2a89c1c64411b
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-04-21 12:46:03 +0200

    regular expresion adjustment

--- attitude_stat.py
+++ attitude_stat.py
@@ -91,7 +91,7 @@ all_files = sorted(glob.glob(os.path.join(input_path + "/ARDUINO/final_daily_fil
 file_list = []
 for f in all_files:
     # match anything (.*) and the date group (?P<date>) consisting of 8 digits (\d{8})
-    match = re.match(r"(?P<date>\d{8}).*", f)
+    match = re.match(r".*(?P<date>\d{8}).*", f)
     # convert string to datetime
     date_from_file = datetime.datetime.strptime(match.group('date'), '%Y%m%d')
     if begin_dt <= date_from_file <= end_dt:
