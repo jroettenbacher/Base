f08b356c5f69bc5a15ca73558a5d86e616395355
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-04-21 13:31:09 +0200

    removed fig size argument

--- attitude_stat.py
+++ attitude_stat.py
@@ -267,7 +267,7 @@ hfmt = mdates.DateFormatter('%d/%m/%y %H')
 # plot all three instrument measurements in one plot
 variables = ["Roll [°]", "Pitch [°]"]
 for var, radar_var in zip(variables, [radar_roll, radar_pitch]):
-    fig, ax = plt.subplots(figsize=(11/2.54, 3.5/2.54))
+    fig, ax = plt.subplots()
     ax.set_title(f"{var[:-4]} Motion \n "
                  f"EUREC4A - RV-Meteor {begin_dt:%Y-%m-%d} - {end_dt:%Y-%m-%d}")
     ax.plot(rv_meteor[var], 'g', label='Seapath 10Hz')
