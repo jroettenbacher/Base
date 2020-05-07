05c6fd55a6494c191242a8d7e874ebc4b74b7995
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-05-07 20:30:03 +0200

    changed way to assign encoding of time

--- ceilometer_time_shift.py
+++ ceilometer_time_shift.py
@@ -54,10 +54,9 @@ correction = time_sub[2] - time_sub[1] - time_res  # calculate time shift for co
 time = ds.time.values.copy()  # extract time variable from data set
 time[0:indices[2]] = time[0:indices[2]] + correction  # move time skip to beginning of Jan 16
 ds = ds.assign_coords(time=time)
-ds["time"] = ds.time.assign_attrs({'units': "seconds since 1904-01-01 00:00:00.000 00:00",
-                                   'calendar': "standard",
-                                   'long_name': "time UTC",
+ds["time"] = ds.time.assign_attrs({'long_name': "time UTC",
                                    'axis': "T"})
+ds["time"].encoding = {'units': "seconds since 1904-01-01 00:00:00.000 00:00", 'calendar': "standard"}
 ds = ds.assign_attrs(comment="This file was corrected for a time lag. It was lagging behind 348 seconds. "
                              "That error was corrected on Jan 26 04:56:46 UTC, which introduced a time skip. "
                              "This time skip was moved to the beginning of Jan 16. For further information contact: "
