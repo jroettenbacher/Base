6289faeb67f8a6a4e6e2fb613c32cf15530da86d
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-06-11 12:03:50 +0200

    commented not zoomed in plots out

--- heave_correction.py
+++ heave_correction.py
@@ -18,8 +18,8 @@ import pandas as pd
 
 # run heave correction on cloudnet input file
 larda = pyLARDA.LARDA().connect('eurec4a', build_lists=True)
-begin_dt = dt.datetime(2020, 1, 28, 0, 0, 5)
-end_dt = dt.datetime(2020, 1, 28, 23, 59, 55)
+begin_dt = dt.datetime(2020, 2, 10, 0, 0, 5)
+end_dt = dt.datetime(2020, 2, 10, 23, 59, 55)
 plot_range = [0, 'max']
 mdv = larda.read("LIMRAD94_cn_input", "Vel", [begin_dt, end_dt], plot_range)
 moments = {"VEL": mdv}
@@ -46,17 +46,18 @@ print("Done with heave correction")
 ########################################################################################################################
 plot_path = "/projekt1/remsens/work/jroettenbacher/plots/heave_correction"
 plot_range = [0, 3000]
-begin_dt_zoom = dt.datetime(2020, 1, 28, 17, 12, 0)
-end_dt_zoom = dt.datetime(2020, 1, 28, 17, 22, 0)
+begin_dt_zoom = dt.datetime(2020, 2, 10, 21, 55, 0)
+end_dt_zoom = dt.datetime(2020, 2, 10, 22, 20, 0)
 name = f'{plot_path}/{begin_dt:%Y%m%d_%H%M}_{end_dt:%Y%m%d_%H%M}_{plot_range[1] / 1000:.0f}km_cloudnet_input_meanHR'
 name_zoom = f'{plot_path}/{begin_dt_zoom:%Y%m%d_%H%M}_{end_dt_zoom:%Y%m%d_%H%M}_{plot_range[1] / 1000:.0f}km_cloudnet_input_meanHR'
+
 mdv['var_lims'] = [-7, 7]
-# uncorrected MDV
-fig, _ = pyLARDA.Transformations.plot_timeheight(mdv, rg_converter=False, title=True, range_interval=plot_range)
-fig_name = name + '_MDV_uncorrected.png'
-fig.savefig(fig_name, dpi=250)
-plt.close()
-print(f'figure saved :: {fig_name}')
+# # uncorrected MDV
+# fig, _ = pyLARDA.Transformations.plot_timeheight(mdv, rg_converter=False, title=True, range_interval=plot_range)
+# fig_name = name + '_MDV_uncorrected.png'
+# fig.savefig(fig_name, dpi=250)
+# plt.close()
+# print(f'figure saved :: {fig_name}')
 
 # uncorrected MDV zoom
 fig, _ = pyLARDA.Transformations.plot_timeheight(mdv, rg_converter=False, title=True,
@@ -69,12 +70,12 @@ print(f'figure saved :: {fig_name}')
 
 Vel_cor = moments['Vel_cor']
 Vel_cor['var_lims'] = [-7, 7]
-# corrected MDV
-fig, _ = pyLARDA.Transformations.plot_timeheight(Vel_cor, rg_converter=False, title=True, range_interval=plot_range)
-fig_name = name + '_MDV_corrected.png'
-fig.savefig(fig_name, dpi=250)
-plt.close()
-print(f'figure saved :: {fig_name}')
+# # corrected MDV
+# fig, _ = pyLARDA.Transformations.plot_timeheight(Vel_cor, rg_converter=False, title=True, range_interval=plot_range)
+# fig_name = name + '_MDV_corrected.png'
+# fig.savefig(fig_name, dpi=250)
+# plt.close()
+# print(f'figure saved :: {fig_name}')
 
 # corrected MDV zoom
 fig, _ = pyLARDA.Transformations.plot_timeheight(Vel_cor, rg_converter=False, title=True, range_interval=plot_range,
@@ -86,12 +87,12 @@ print(f'figure saved :: {fig_name}')
 
 cor_meas = moments['Vel_cor-Vel']
 cor_meas['var_lims'] = [-1, 1]
-# corrected MDV - measured MDV
-fig, _ = pyLARDA.Transformations.plot_timeheight(cor_meas, rg_converter=False, title=True, range_interval=plot_range)
-fig_name = name + '_MDV_corrected-measured.png'
-fig.savefig(fig_name, dpi=250)
-plt.close()
-print(f'figure saved :: {fig_name}')
+# # corrected MDV - measured MDV
+# fig, _ = pyLARDA.Transformations.plot_timeheight(cor_meas, rg_converter=False, title=True, range_interval=plot_range)
+# fig_name = name + '_MDV_corrected-measured.png'
+# fig.savefig(fig_name, dpi=250)
+# plt.close()
+# print(f'figure saved :: {fig_name}')
 
 # corrected MDV - measured MDV zoom
 fig, _ = pyLARDA.Transformations.plot_timeheight(cor_meas, rg_converter=False, title=True, range_interval=plot_range,
@@ -101,31 +102,31 @@ fig.savefig(fig_name, dpi=250)
 plt.close()
 print(f'figure saved :: {fig_name}')
 
-meas_cor = moments['Vel-Vel_cor']
-meas_cor['var_lims'] = [-1, 1]
-# measured MDV - corrected MDV
-fig, _ = pyLARDA.Transformations.plot_timeheight(meas_cor, rg_converter=False, title=True, range_interval=plot_range)
-fig_name = name + '_MDV_measured-corrected.png'
-fig.savefig(fig_name, dpi=250)
-plt.close()
-print(f'figure saved :: {fig_name}')
+# meas_cor = moments['Vel-Vel_cor']
+# meas_cor['var_lims'] = [-1, 1]
+# # measured MDV - corrected MDV
+# fig, _ = pyLARDA.Transformations.plot_timeheight(meas_cor, rg_converter=False, title=True, range_interval=plot_range)
+# fig_name = name + '_MDV_measured-corrected.png'
+# fig.savefig(fig_name, dpi=250)
+# plt.close()
+# print(f'figure saved :: {fig_name}')
+#
+# # measured MDV - corrected MDV zoom
+# fig, _ = pyLARDA.Transformations.plot_timeheight(meas_cor, rg_converter=False, title=True, range_interval=plot_range,
+#                                                  time_interval=[begin_dt_zoom, end_dt_zoom])
+# fig_name = name_zoom + '_MDV_measured-corrected.png'
+# fig.savefig(fig_name, dpi=250)
+# plt.close()
+# print(f'figure saved :: {fig_name}')
 
-# measured MDV - corrected MDV zoom
-fig, _ = pyLARDA.Transformations.plot_timeheight(meas_cor, rg_converter=False, title=True, range_interval=plot_range,
-                                                 time_interval=[begin_dt_zoom, end_dt_zoom])
-fig_name = name_zoom + '_MDV_measured-corrected.png'
-fig.savefig(fig_name, dpi=250)
-plt.close()
-print(f'figure saved :: {fig_name}')
-
-heave_corr = moments["heave_corr"]
-heave_corr['var_lims'] = [-1, 1]
-# heave correction
-fig, _ = pyLARDA.Transformations.plot_timeheight(heave_corr, rg_converter=False, title=True, range_interval=plot_range)
-fig_name = name + '_heave_correction.png'
-fig.savefig(fig_name, dpi=250)
-plt.close()
-print(f'figure saved :: {fig_name}')
+# heave_corr = moments["heave_corr"]
+# heave_corr['var_lims'] = [-1, 1]
+# # heave correction
+# fig, _ = pyLARDA.Transformations.plot_timeheight(heave_corr, rg_converter=False, title=True, range_interval=plot_range)
+# fig_name = name + '_heave_correction.png'
+# fig.savefig(fig_name, dpi=250)
+# plt.close()
+# print(f'figure saved :: {fig_name}')
 
 # heave correction zoom
 fig, _ = pyLARDA.Transformations.plot_timeheight(heave_corr, rg_converter=False, title=True, range_interval=plot_range,
