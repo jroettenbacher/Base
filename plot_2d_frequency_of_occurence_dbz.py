5e3c977216a836c40a565b716119ca4a096c6e47
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-06-24 09:55:09 +0200

    new plotting script for 2d frequency of occurence plot of dBZ

---
+++ plot_2d_frequency_of_occurence_dbz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""script to plot a height resolved frequency of occurence plot of dBZ values
+input: Ze from larda
+output: 2d frequency of occurence plot
+author: Johannes Roettenbacher
+"""
+
+# read in dBZ values
+
+#
