2c0543cdb771749d7f760fb8d40976ef35b0712f
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-07-28 16:12:29 +0200

    added a step option to plot only certain hours of the day

--- plot_2d_frequency_of_occurence_dbz.py
+++ plot_2d_frequency_of_occurence_dbz.py
@@ -14,13 +14,14 @@ import datetime as dt
 import time
 import numpy as np
 import functions_jr as jr
+import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 from matplotlib.ticker import AutoMinorLocator
 from matplotlib.ticker import IndexFormatter
 
 
-def calc_freq_of_occurrence_reflectivity(radar_ze, bin_width=1):
+def calc_freq_of_occurrence_reflectivity(radar_ze, indices, bin_width=1):
     """Calculate the frequency of occurrence of reflectivity per height bin
 
     Args:
@@ -43,8 +44,8 @@ def calc_freq_of_occurrence_reflectivity(radar_ze, bin_width=1):
     hist_range = (-60, 30)  # upper and lower bin boundary
     density = False  # compute the probability density function
     for i in range(radar_ze['var'].shape[1]):
-        h_bin = h.lin2z(radar_ze['var'][:, i])  # select height bin and convert to dBZ
-        mask = radar_ze['mask'][:, i]  # get mask for height bin
+        h_bin = h.lin2z(radar_ze['var'][indices, i])  # select height bin and convert to dBZ
+        mask = radar_ze['mask'][indices, i]  # get mask for height bin
         h_bin[mask] = np.nan  # set -999 to nan
         tmp_hist, _ = np.histogram(h_bin[~np.isnan(h_bin)], bins=hist_bins, range=hist_range, density=density)
         hist.append(tmp_hist)
@@ -60,11 +61,12 @@ def calc_freq_of_occurrence_reflectivity(radar_ze, bin_width=1):
     return foc_array, hist_bins
 
 
-def plot_frequency_of_occurence_LIMRAD94(program, plot_path, larda_system):
+def plot_frequency_of_occurence_LIMRAD94(program, step, plot_path, larda_system):
     """Plot a 2D frequency of occurrence of reflectivity over height
 
     Args:
         program (list): list of chirp program numbers like 'P07'
+        step (int): at which time step the plots should be made, allows for daily cycle plots (1, 2, 3, 4, 6, 8, 12, 24)
         plot_path (str): path where plot should be saved to
         larda_system (str): which larda system to use (LIMRAD94, LIMRAD94_cn_input), should have Ze and rg parameter
 
@@ -79,6 +81,12 @@ def plot_frequency_of_occurence_LIMRAD94(program, plot_path, larda_system):
     end_dts = {'P09': dt.datetime(2020, 1, 27, 0, 0, 5), 'P06': dt.datetime(2020, 1, 30, 23, 42, 00),
                'P07': dt.datetime(2020, 2, 19, 23, 59, 55)}
     program_names = {'P09': "tradewindCU (P09)", 'P06': "Cu_small_Tint (P06)", 'P07': "Cu_small_Tint2 (P07)"}
+
+    # get time chunk borders
+    assert step in [1, 2, 3, 4, 6, 8, 12, 24], f"Step size is not a sensible value, {step}!" \
+                                               f"Use an integer which 24 can be divided by!"
+    steps = np.arange(0, 25, step)
+
 
     # get mean sensitivity limits
     mean_sl = jr.calc_sensitivity_curve(program)
@@ -98,52 +106,65 @@ def plot_frequency_of_occurence_LIMRAD94(program, plot_path, larda_system):
         # read in linear reflectivity factor from larda and convert to dBZ
         radar_ze = larda.read(system, "Ze", [begin_dt, end_dt], plot_range)
         print(f"Read in data from larda: {time.time() - start:.2f} seconds")
-        foc_array, hist_bins = calc_freq_of_occurrence_reflectivity(radar_ze)
 
-        # scale sensitivity limit to pcolormesh axis = 0-{number of hist_bins}, add lowest value in histogram bins
-        mean_slh = h.lin2z(mean_sl['mean_slh'][p]) + np.abs(hist_bins[0])
-        mean_slv = h.lin2z(mean_sl['mean_slv'][p]) + np.abs(hist_bins[0])
+        # convert time stamps to datetime objects and create a pandas series
+        dts = [h.ts_to_dt(t) for t in radar_ze['ts']]
+        tmp_series = pd.Series(np.ones_like(radar_ze['ts']), index=dts)
+        hours = tmp_series.index.hour  # get hours from index
 
-        figname = f"{plot_path}/RV-Meteor_{system}_freq_of_occurrence_{p}_{begin_dt:%Y%m%d}-{end_dt:%Y%m%d}.png"
+        for i in range(len(steps)-1):
+            # define a selector to only select hours in between step size
+            selector = (steps[i] <= hours) & (hours < steps[i+1])
+            # get indices of those time stamps
+            indices = [tmp_series.index.get_loc(t) for t in tmp_series[selector].index]
 
-        # create an array for the x and y tick labels
-        height = radar_ze['rg']
-        ylabels = np.floor(height) // 100 * 100 / 1000
-        xlabels = hist_bins
+            foc_array, hist_bins = calc_freq_of_occurrence_reflectivity(radar_ze, indices)
 
-        # create title
-        title = f"Frequency of Occurrence of Reflectivity {system}" \
-                f"\nEUREC4A {begin_dt:%Y-%m-%d} - {end_dt:%Y-%m-%d}" \
-                f"\n Chirp program: {program_names[p]}"
+            # scale sensitivity limit to pcolormesh axis = 0-{number of hist_bins}, add lowest value in histogram bins
+            mean_slh = h.lin2z(mean_sl['mean_slh'][p]) + np.abs(hist_bins[0])
+            mean_slv = h.lin2z(mean_sl['mean_slv'][p]) + np.abs(hist_bins[0])
 
-        fig, ax = plt.subplots()
-        im = ax.pcolormesh(foc_array, cmap='jet', norm=LogNorm())
-        ax.plot(mean_slh, np.arange(len(height)), "k-", label="Horizontal Polarization")
-        ax.plot(mean_slv, np.arange(len(height)), "r-", label="Vertical Polarization")
-        fig.colorbar(im, ax=ax)
-        fig.legend(title="Mean Sensitivity Limit", bbox_to_anchor=(0.5, -0.01), loc="lower center",
-                   bbox_transform=fig.transFigure, ncol=2)
-        ax.yaxis.set_minor_locator(AutoMinorLocator(5))
-        ax.xaxis.set_minor_locator(AutoMinorLocator(2))
-        ax.xaxis.set_major_formatter(IndexFormatter(xlabels))
-        ax.yaxis.set_major_formatter(IndexFormatter(ylabels))
-        ax.tick_params(which='minor', length=4, top=True, right=True)
-        ax.tick_params(which='major', length=6)
-        ax.xaxis.grid(True, which='major', color="k", linestyle='-', linewidth=1, alpha=0.5)
-        ax.yaxis.grid(True, which='major', color="k", linestyle='-', linewidth=1, alpha=0.5)
-        ax.set_xlabel("Reflectivity [dBZ]")
-        ax.set_ylabel("Height [km]")
-        ax.set_title(title)
-        fig.tight_layout()
-        fig.subplots_adjust(bottom=0.2)
-        fig.savefig(figname, dpi=300)
-        plt.close()
+            figname = f"{plot_path}/RV-Meteor_{system}_freq_of_occurrence_{p}_{begin_dt:%Y%m%d}-{end_dt:%Y%m%d}_{steps[i]}-{steps[i+1]}UTC.png"
+
+            # create an array for the x and y tick labels
+            height = radar_ze['rg']
+            ylabels = np.floor(height) // 100 * 100 / 1000
+            xlabels = hist_bins
+
+            # create title
+            title = f"Frequency of Occurrence of Reflectivity {system}" \
+                    f"\nEUREC4A {begin_dt:%Y-%m-%d} - {end_dt:%Y-%m-%d} {steps[i]} - {steps[i+1]} UTC" \
+                    f"\n Chirp program: {program_names[p]}"
+
+            fig, ax = plt.subplots()
+            im = ax.pcolormesh(foc_array, cmap='jet', norm=LogNorm())
+            ax.plot(mean_slh, np.arange(len(height)), "k-", label="Horizontal Polarization")
+            ax.plot(mean_slv, np.arange(len(height)), "r-", label="Vertical Polarization")
+            fig.colorbar(im, ax=ax)
+            fig.legend(title="Mean Sensitivity Limit", bbox_to_anchor=(0.5, -0.01), loc="lower center",
+                       bbox_transform=fig.transFigure, ncol=2)
+            ax.yaxis.set_minor_locator(AutoMinorLocator(5))
+            ax.xaxis.set_minor_locator(AutoMinorLocator(2))
+            ax.xaxis.set_major_formatter(IndexFormatter(xlabels))
+            ax.yaxis.set_major_formatter(IndexFormatter(ylabels))
+            ax.tick_params(which='minor', length=4, top=True, right=True)
+            ax.tick_params(which='major', length=6)
+            ax.xaxis.grid(True, which='major', color="k", linestyle='-', linewidth=1, alpha=0.5)
+            ax.yaxis.grid(True, which='major', color="k", linestyle='-', linewidth=1, alpha=0.5)
+            ax.set_xlabel("Reflectivity [dBZ]")
+            ax.set_ylabel("Height [km]")
+            ax.set_title(title)
+            fig.tight_layout()
+            fig.subplots_adjust(bottom=0.2)
+            fig.savefig(figname, dpi=300)
+            plt.close()
 
 
 if __name__ == '__main__':
-    program = ['P06', 'P07', 'P09']
+    program = ['P07']
+    step = 3
     plot_path = "../plots/foc_LIMRAD94"
     larda_systems = ["LIMRAD94", "LIMRAD94_cn_input"]
 
     for larda_system in larda_systems:
-        plot_frequency_of_occurence_LIMRAD94(program, plot_path, larda_system)
+        plot_frequency_of_occurence_LIMRAD94(program, step, plot_path, larda_system)
