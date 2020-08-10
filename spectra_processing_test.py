3aa7908557eb841ce677ec964c21195d77a01d1a
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-08-10 13:28:31 +0200

    added two rudimentary plots and new time spans

--- spectra_processing_test.py
+++ spectra_processing_test.py
@@ -6,13 +6,31 @@ sys.path.append('.')
 
 import pyLARDA
 from larda.pyLARDA.SpectraProcessing import load_spectra_rpgfmcw94
+import matplotlib.pyplot as plt
+import numpy as np
 
 larda = pyLARDA.LARDA().connect('eurec4a', build_lists=True)
-begin_dt = datetime.datetime(2020, 2, 3, 0, 0, 5)
-end_dt = datetime.datetime(2020, 2, 3, 23, 59, 55)
+# begin_dt = datetime.datetime(2020, 2, 3, 0, 0, 5)
+# end_dt = datetime.datetime(2020, 2, 3, 23, 59, 55)
+# begin_dt_zoom = datetime.datetime(2020, 2, 3, 18, 0, 0)
+# end_dt_zoom = datetime.datetime(2020, 2, 3, 18, 20, 0)
+begin_dt = datetime.datetime(2020, 2, 5, 0, 0, 5)
+end_dt = datetime.datetime(2020, 2, 5, 23, 59, 55)
+begin_dt_zoom = datetime.datetime(2020, 2, 5, 9, 25, 0)
+end_dt_zoom = datetime.datetime(2020, 2, 5, 9, 35, 0)
 timespan = [begin_dt, end_dt]
-begin_dt_zoom = datetime.datetime(2020, 2, 3, 18, 0, 0)
-end_dt_zoom = datetime.datetime(2020, 2, 3, 18, 20, 0)
 timespan_zoom = [begin_dt_zoom, end_dt_zoom]
-container = load_spectra_rpgfmcw94(larda, timespan,
-                                   heave_correction=True)
+container = load_spectra_rpgfmcw94(larda, timespan_zoom, heave_correction=True)
+container_noheave = load_spectra_rpgfmcw94(larda, timespan_zoom)
+
+spectrum = container['VHSpec'].copy()
+spectrum['var'] = np.ma.masked_where(spectrum['var'][100, 50, :] == -999, spectrum['var'][100, 50, :])
+plt.plot(spectrum['var'])
+plt.savefig(f'../plots/spectra_heave_cor.png')
+plt.close()
+
+spectrum = container_noheave['VHSpec'].copy()
+spectrum['var'] = np.ma.masked_where(spectrum['var'][100, 50, :] == -999, spectrum['var'][100, 50, :])
+plt.plot(spectrum['var'])
+plt.savefig(f'../plots/spectra_no_heave_cor.png')
+plt.close()
