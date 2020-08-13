747db23a4137fd3fe11c711fdd33d1467d16781a
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-08-13 10:21:26 +0200

    added path variable to not overwrite heave corrected nc files

--- loop_create_limrad94_calibrated_2.sh
+++ loop_create_limrad94_calibrated_2.sh
@@ -6,6 +6,6 @@ for date in {201..229}; do
 	echo "##################################################"
 	echo "#     STARTING DATE 20200${date}                 #"
 	echo "##################################################"
-	/home/jroettenbacher/envs/jr_base/bin/python3 /projekt1/remsens/work/jroettenbacher/Base/create_limrad94_calibrated.py date=20200${date} > /projekt1/remsens/work/jroettenbacher/Base/log/create_limrad94_calibrated_200${date}.log 2>&1
+	/home/jroettenbacher/envs/jr_base/bin/python3 /projekt1/remsens/work/jroettenbacher/Base/create_limrad94_calibrated.py date=20200${date} path='/projekt2/remsens/data_new/site-campaign/rv_meteor-eurec4a/instruments/LIMRAD94/cloudnet_input_new_processing' > /projekt1/remsens/work/jroettenbacher/Base/log/create_limrad94_calibrated_200${date}.log 2>&1
 	echo "Done with 20200${date}"
 done
