d2e30423ed08cf475ca2d430781051708695c78e
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-09-08 15:46:15 +0200

    removed import of git, because I cannot install it to my environment

--- larda/pyLARDA/NcWrite.py
+++ larda/pyLARDA/NcWrite.py
@@ -5,7 +5,7 @@ import netCDF4
 import numpy as np
 import pyLARDA.helpers as h
 import time
-import git
+# import git
 
 
 def export_spectra2nc(data, larda_git_path='', system='', path='', **kwargs):
