911020afc44b8aff7567102caa6f525308bdc218
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-09-08 15:18:09 +0200

    added functions for time shift analysis, needed to import a few more functions and modules

--- larda/pyLARDA/SpectraProcessing.py
+++ larda/pyLARDA/SpectraProcessing.py
@@ -23,11 +23,13 @@ import time
 from itertools import product
 from numba import jit
 from tqdm.auto import tqdm
+import matplotlib.pyplot as plt
+from scipy.signal import correlate
 
 warnings.simplefilter("ignore", UserWarning)
 sys.path.append('../../larda/')
 
-from pyLARDA.helpers import z2lin, argnearest, lin2z, ts_to_dt
+from pyLARDA.helpers import z2lin, argnearest, lin2z, ts_to_dt, dt_to_ts
 
 logger = logging.getLogger(__name__)
 
@@ -958,12 +960,12 @@ def heave_correction_spectra(data, date,
                              mean_hr=True, only_heave=False, use_cross_product=True, transform_to_earth=True, add=False):
     """Shift Doppler spectra to correct for heave motion of ship (RV-Meteor)
     Calculate heave rate from seapath measurements and create heave correction array. Translate the heave correction to
-    a number of spectra bins by which to move each spectra. If Spectra are given, shift them and return a 3D array with
-    the  shifted spectra.
-    Without spectra input, only the heave correction array and the array with the number of bins to move is returned.
+    a number spectra bins by which to move each spectra. If Spectra are given, shift them and return a 3D array with the
+    shifted spectra.
+    Without spectra input, only the heave correction array and the array with the number if bins to move is returned.
 
     Args:
-        data: LIMRAD94 data container filled with spectra and C1/2/3_Range, SeqIntTime, MaxVel, DoppLen, ts from LV1 file
+        data: LIMRAD94 data container filled with spectra and C1/2/3_Range, SeqIntTime, MaxVel, DoppLen from LV1 file
         date (datetime.datetime): object with date of current file
         path_to_seapath (string): path where seapath measurement files (daily dat files) are stored
         mean_hr (bool): whether to use the mean heave rate over the SeqIntTime or the heave rate at the start time of the chirp
@@ -973,7 +975,7 @@ def heave_correction_spectra(data, date,
         add (bool): whether to add the heave rate or subtract it
 
     Returns: A number of variables
-        new_spectra (ndarray); corrected Doppler spectra, same shape as data["VHSpec"]["var"] or list if no Doppler
+        new_spectra (ndarray); corrected Doppler velocities, same shape as data["VHSpec"]["var"] or list if no Doppler
         Spectra are given;
         heave_corr (ndarray): heave rate closest to each radar timestep for each height bin, shape = (time x range);
         seapath_out (pd.DataFrame): data frame with all heave information from the closest time steps to the chirps
@@ -991,6 +993,12 @@ def heave_correction_spectra(data, date,
     ####################################################################################################################
     seapath = calc_heave_rate(seapath, only_heave=only_heave, use_cross_product=use_cross_product,
                               transform_to_earth=transform_to_earth)
+
+    ####################################################################################################################
+    # Calculating time shift between seapath and radar time, shift seapath data by calculated shift
+    ####################################################################################################################
+    time_shift, shift, _ = calc_time_shift_limrad_seapath(seapath)
+    seapath = shift_seapath(seapath, shift)
 
     ####################################################################################################################
     # Calculating heave correction array and translate to number of Doppler bin shifts
@@ -1021,13 +1029,7 @@ def heave_correction_spectra(data, date,
                 # loop through range gates
                 # TODO: check if mask is True and skip, although masked shifted spectra do not introduce any error,
                 # this might speed up things...
-                try:
-                    shift = int(n_dopp_bins_shift[iT, iR])
-                except ValueError as e:
-                    logger.debug(f'A {e.__class__} occurred. \n'
-                                 f'Value of n_dopp_bins_shift[{iT}, {iR}]: {n_dopp_bins_shift[iT, iR]} \n'
-                                 f'shift is set to 0')
-                    shift = 0
+                shift = int(n_dopp_bins_shift[iT, iR])
                 spectrum = spectra[iT, iR, :]
                 if add:
                     new_spec = np.roll(spectrum, shift)
@@ -1333,6 +1335,171 @@ def heave_rate_to_spectra_bins(heave_corr, doppler_res):
     return n_dopp_bins_shift, heave_corr
 
 
+def calc_time_shift_limrad_seapath(seapath, version=1, plot_xcorr=False):
+    """Calculate time shift between LIMRAD94 mean Doppler veloctiy and heave rate of RV Meteor
+
+    Average the mean Doppler velocity over the whole range and interpolate the heave rate onto the radar time.
+    Use a cross correlation method as described on stackoverflow to retrieve the time shift between the two time series.
+    Version 1: https://stackoverflow.com/a/13830177
+    Version 2: https://stackoverflow.com/a/56432463
+    Both versions return the same time shift to the 3rd decimal.
+
+    Args:
+        seapath (pd.DataFrame): data frame with heave rate of RV-Meteor
+        version (int): which version to use 1 or 2
+        plot_xcorr (bool): plot cross correlation function in temporary plot folder
+
+    Returns: time shift in seconds between the two timeseries
+
+    """
+    start = time.time()
+    import pyLARDA
+    plot_path = "/projekt1/remsens/work/jroettenbacher/Base/tmp"
+    logger.debug(f"plot path: {plot_path}")
+    # get date from seapath data
+    begin_dt = seapath.index[0]
+    end_dt = begin_dt + datetime.timedelta(seconds=23 * 60 * 60 + 59 * 60 + 59)
+    logger.info(f"Start time shift analysis between LIMRAD94 mean Doppler velocity and RV Meteor heave rate for "
+                f"{begin_dt:%Y-%m-%d}")
+    plot_range = [0, 'max']
+    larda = pyLARDA.LARDA().connect('eurec4a')
+    ####################################################################################################################
+    # read in data
+    ####################################################################################################################
+    # read in radar doppler velocity
+    radar_vel = larda.read("LIMRAD94_cn_input", "Vel", [begin_dt, end_dt], plot_range)
+    # needed if time shift should be calculated for each chirp separately, leave in for now
+    # data = dict()
+    # for var in ['C1Range', 'C2Range', 'C3Range']:
+    #     data.update({var: larda.read('LIMRAD94', var, [begin_dt, end_dt], plot_range)})
+    # range_bins = jr.get_range_bin_borders(3, data)
+
+    # set masked values in var to nan
+    vel = radar_vel['var']
+    vel[vel.mask] = np.nan
+    vel = np.asarray(vel)  # convert to ndarray
+    # average of mean doppler velocity over height
+    vel_mean = np.nanmean(vel, axis=1)
+
+    ####################################################################################################################
+    # select closest seapath values to each radar time step
+    ####################################################################################################################
+    seapath_ts = seapath.index.values.astype(np.float64) / 10 ** 9  # convert datetime index to seconds since 1970-01-01
+    id_diff_mins = []  # initialize list for indices of the time steps with minimum difference
+    means_ls = []  # initialize list for heave rate value closest to each radar time step
+    for t in radar_vel['ts']:
+        id_diff_min = argnearest(seapath_ts, t)  # find index of nearest seapath time step to radar time step
+        id_diff_mins.append(id_diff_min)
+        # get time stamp of closest index
+        ts_id_diff_min = seapath.index[id_diff_min]
+        try:
+            means_ls.append(seapath.loc[ts_id_diff_min])
+        except KeyError:
+            logger.info('Timestamp out of bounds of heave rate time series')
+
+    # concatenate all values into one dataframe with the original header (transpose)
+    seapath_closest = pd.concat(means_ls, axis=1).T
+
+    ####################################################################################################################
+    # interpolate heave rate to radar time
+    ####################################################################################################################
+    # extract heave rate
+    heave_rate = seapath_closest['Heave Rate [m/s]']
+    radar_time = radar_vel['ts']
+    seapath_time = np.asarray([dt_to_ts(t) for t in seapath_closest.index])
+    heave_rate_rts = np.interp(radar_time, seapath_time, heave_rate.values)  # heave rate at radar time
+
+    ####################################################################################################################
+    # interpolate over nan values
+    ####################################################################################################################
+    # check for nan values and interpolate them
+    # heave rate
+    if any(np.isnan(heave_rate_rts)):
+        idx_not_nan = np.asarray(~np.isnan(heave_rate_rts)).nonzero()[0]
+        idx_to_ip = np.arange(len(heave_rate_rts))
+        heave_rate_ip = np.interp(idx_to_ip, idx_to_ip[idx_not_nan], heave_rate_rts[idx_not_nan])
+    else:
+        heave_rate_ip = heave_rate_rts
+
+    # Doppler velocity
+    if any(np.isnan(vel_mean)):
+        idx_not_nan = np.asarray(~np.isnan(vel_mean)).nonzero()[0]
+        idx_to_ip = np.arange(len(vel_mean))
+        vel_ip = np.interp(idx_to_ip, idx_to_ip[idx_not_nan], vel_mean[idx_not_nan])
+    else:
+        vel_ip = vel_mean
+
+    n = vel_ip.size
+    sr = n / (24 * 60 * 60)  # number of samples per day / seconds per day -> sampling rate
+    logger.info(f"Using version {version} for cross correlation...")
+    if version == 1:
+        xcorr = np.correlate(heave_rate_ip, vel_ip, 'full')
+        dt_lags = np.arange(1 - n, n)
+        time_shift = float(dt_lags[xcorr.argmax()]) / sr
+        if plot_xcorr:
+            plt.plot(dt_lags, xcorr)
+            plt.savefig(f"{plot_path}/RV-Meteor_cross_corr_version1_mean-V-dop_heave-rate_{begin_dt:%Y-%m-%d}.png")
+            plt.close()
+    elif version == 2:
+        y2 = heave_rate_ip
+        y1 = vel_ip
+        corr = correlate(y2, y1, mode='same') / np.sqrt(correlate(y1, y1, mode='same')[int(n / 2)] * correlate(y2, y2, mode='same')[int(n / 2)])
+        delay_array = np.linspace(-0.5 * n / sr, 0.5 * n / sr, n)
+        time_shift = float(delay_array[np.argmax(corr)])
+        if plot_xcorr:
+            plt.plot(delay_array, corr)
+            plt.savefig(f"{plot_path}/RV-Meteor_cross_corr_version2_mean-V-dop_heave-rate_{begin_dt:%Y-%m-%d}.png")
+            plt.close()
+
+    logger.debug(f"version: {version}")
+    # turn time shift in number of time steps
+    sr = 1 / (np.median(np.diff(seapath.index)).astype('float') * 10**-9)  # sampling rate in Hertz
+    shift = int(np.round(time_shift * sr))
+    logger.info(f"Found time shift to be {time_shift:.3f} seconds")
+    logger.info(f"Done with cross correlation, elapsed time = {seconds_to_fstring(time.time() - start)} [min:sec]")
+    return time_shift, shift, seapath
+
+
+def shift_seapath(seapath, shift):
+    """Shift seapath values by given shift
+
+    Args:
+        seapath (pd.Dataframe): Dataframe with heave motion of RV-Meteor
+        shift (int): number of time steps to shift data
+
+    Returns: shifted Dataframe
+
+    """
+    start = time.time()
+    logger.info(f"Shifting seapath data by {shift} time steps.")
+    # get day of seapath data
+    datetime = seapath.index[0]
+    # shift seapath data by shift
+    seapath_shifted = seapath.shift(periods=shift)
+
+    # replace Nans at start with data from the previous day or from following day
+    if shift > 0:
+        datetime_previous = datetime - datetime.timedelta(1)  # get date of previous day
+        skiprows = np.arange(1, len(seapath) - shift + 2)  # define rows to skip on read in
+        # read in one more row for heave rate calculation
+        seapath_previous = read_seapath(datetime_previous, nrows=shift + 1, skiprows=skiprows)
+        seapath_previous = calc_heave_rate(seapath_previous)
+        seapath_previous = seapath_previous.iloc[1:, :]  # remove first row (=nan)
+        # remove index and replace with index from original data frame
+        seapath_previous = seapath_previous.reset_index(drop=True).set_index(seapath_shifted.iloc[0:shift, :].index)
+        seapath_shifted.update(seapath_previous)  # overwrite nan values in shifted data frame
+    else:
+        datetime_following = datetime + datetime.timedelta(1)  # get date from following day
+        seapath_following = read_seapath(datetime_following, nrows=np.abs(shift))
+        seapath_following = calc_heave_rate(seapath_following)
+        # overwrite nan values
+        # leaves in one NaN value because the heave rate of the first time step of a day cannot be calculated
+        # one nan is better than many (shift) though, so this is alright
+        seapath_following = seapath_following.reset_index(drop=True).set_index(seapath_shifted.iloc[shift:, :].index)
+        seapath_shifted.update(seapath_following)  # overwrite nan values in shifted data frame
+
+    logger.info(f"Done with shifting seapath data, elapsed time = {seconds_to_fstring(time.time() - start)} [min:sec]")
+    return seapath_shifted
 
 
 
