92992f7eaeaa41e6809fa65e55758388bc1f3ec6
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-10-09 13:31:07 +0200

    bug fix in dummy dates, added missing program name to selection of statistical values, added second df with sensitivity values in dBZ

--- make_sensitivity_csv.py
+++ make_sensitivity_csv.py
@@ -6,6 +6,7 @@ import sys
 sys.path.append('/projekt1/remsens/work/jroettenbacher/Base/larda')  # append path to larda
 sys.path.append('.')
 import pyLARDA
+from pyLARDA.helpers import lin2z
 import functions_jr as jr
 import datetime as dt
 import pandas as pd
@@ -29,15 +30,21 @@ if __name__ == '__main__':
     # get mean sensitivity limits
     stats_sl = jr.calc_sensitivity_curve(program, campaign, rain_flag=True)
     # get height bins for corresponding chirptable
-    dummy_dates = {'P07': dt.datetime(2020, 1, 25, 0, 0, 5), 'P09': dt.datetime(2020, 2, 10, 0, 0, 5)}
+    dummy_dates = {'P09': dt.datetime(2020, 1, 25, 0, 0, 5), 'P07': dt.datetime(2020, 2, 10, 0, 0, 5)}
     output = dict()  # dictionary for temporary data storage
     for p in program:
         height = larda.read("LIMRAD94", "Ze", [dummy_dates[p]], [0, 'max'])['rg']
         # write output to csv file
-        output[p] = dict(height_m=height, mean_slh=stats_sl['mean_slh'], mean_slh_f=stats_sl['mean_slh_f'],
-                         mean_slv=stats_sl['mean_slv'], mean_slv_f=stats_sl['mean_slv_f'],
-                         median_slh=stats_sl['median_slh'], median_slh_f=stats_sl['median_slh_f'],
-                         median_slv=stats_sl['median_slv'], median_slv_f=stats_sl['median_slv_f'])
+        output[p] = dict(height_m=height, mean_slh=stats_sl['mean_slh'][p], mean_slh_f=stats_sl['mean_slh_f'][p],
+                         mean_slv=stats_sl['mean_slv'][p], mean_slv_f=stats_sl['mean_slv_f'][p],
+                         median_slh=stats_sl['median_slh'][p], median_slh_f=stats_sl['median_slh_f'][p],
+                         median_slv=stats_sl['median_slv'][p], median_slv_f=stats_sl['median_slv_f'][p])
         csv_name = f"{out_path}/RV-Meteor_cloudradar_sensitivity-limit_{begin_dts[p]:%Y%m%d}-{end_dts[p]:%Y%m%d}.csv"
-        pd.DataFrame(output[p]).to_csv(csv_name, index=False, sep=',')
+        df = pd.DataFrame(output[p])
+        df.to_csv(csv_name, index=False, sep=',')
+        # transform linear units to dBZ
+        for var in df.columns[1:]:
+            df[var] = lin2z(df[var].values)
+        csv_name2 = f"{out_path}/RV-Meteor_cloudradar_sensitivity-limit_dBZ_{begin_dts[p]:%Y%m%d}-{end_dts[p]:%Y%m%d}.csv"
+        df.to_csv(csv_name2, index=False, sep=',')
         log.info(f"saved {csv_name}")
