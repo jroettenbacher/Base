f73c0e404a98344ce1fb84a9c30aabbee8834600
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-12-09 17:44:52 +0100

    Turn off dealiasing for testing

--- create_limrad94_calibrated_eurec4a.py
+++ create_limrad94_calibrated_eurec4a.py
@@ -73,7 +73,7 @@ if __name__ == '__main__':
         'despeckle2D': True,  # 2D convolution (5x5 window), removes single non-zero values,
         'ghost_echo_1': False,  # reduces the domain (Nyquist velocity) by ± 2.5 [m/s], when signal > 0 [dBZ] within 200m above antenna
         'ghost_echo_2': True,  # removes curtain like ghost echos
-        'dealiasing': True,  # spectrum de-aliasing
+        'dealiasing': False,  # spectrum de-aliasing
         'heave_correction': False,  # correct for heave motion of ship
         'add': False,  # add or subtract heave rate (move spectra to left or right)
         'shift': 19,  # number of time steps by which to shift seapath data of RV-Meteor
