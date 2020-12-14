e2e83ac4a1271a5becf3a836ca3e7b40618e0e96
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-12-14 21:16:37 +0100

    changed way to deal with no flags in a day

--- compare_LWP_limrad94-limhat.py
+++ compare_LWP_limrad94-limhat.py
@@ -27,10 +27,10 @@ logger.addHandler(logging.StreamHandler())
 larda = pyLARDA.LARDA().connect('eurec4a')
 
 # set date
-dates = pd.date_range(dt.date(2020, 1, 17), dt.date(2020, 2, 29))
+dates = pd.date_range(dt.date(2020, 1, 19), dt.date(2020, 2, 28))
 for begin_dt in dates:
-    # begin_dt = dt.datetime(2020, 2, 16)
-    end_dt = begin_dt + dt.timedelta(1)
+    # begin_dt = dt.datetime(2020, 2, 16, 0, 0, 5)
+    end_dt = begin_dt + dt.timedelta(0.99999)
     time_interval = [begin_dt, end_dt]
 
     radar_lwp = larda.read("LIMRAD94", "LWP", time_interval)
@@ -38,18 +38,20 @@ for begin_dt in dates:
     hatpro_flag = larda.read("HATPRO", "flag", time_interval)
     hatpro_lwp['var'] = hatpro_lwp['var'] * 1000  # conversion to g/m2
     hatpro_lwp['var_unit'] = 'g m-2'
-    # use data of flag, ignore mask set by larda, necessary at days where no flag was set
-    if isinstance(hatpro_flag['var'], np.ma.MaskedArray):
-        hatpro_flag['var'] = np.copy(hatpro_flag['var'].data)
-        hatpro_flag['mask'] = ~hatpro_flag['mask']
 
     # interpolate HATPRO data on radar time
     hatpro_lwp_ip = trans.interpolate1d(hatpro_lwp, new_time=radar_lwp['ts'])
-    hatpro_flag_ip = trans.interpolate1d(hatpro_flag, new_time=radar_lwp['ts'])
+    # check if a flag is set on that day
+    if any(hatpro_flag['var'] > 8):
+        hatpro_flag_ip = trans.interpolate1d(hatpro_flag, new_time=radar_lwp['ts'])
 
-    # mask flagged data
-    hatpro_lwp_ip['var'] = np.ma.masked_where(hatpro_flag_ip['var'] > 8, hatpro_lwp_ip['var'])
-    radar_lwp['var'] = np.ma.masked_where(hatpro_flag_ip['var'] > 8, radar_lwp['var'])
+        # mask flagged data
+        hatpro_lwp_ip['var'] = np.ma.masked_where(hatpro_flag_ip['var'] > 8, hatpro_lwp_ip['var'])
+        radar_lwp['var'] = np.ma.masked_where(hatpro_flag_ip['var'] > 8, radar_lwp['var'])
+        # get position of flags for vertical lines in plot
+        vlines = [h.ts_to_dt(t) for t in hatpro_flag_ip['ts'][hatpro_flag_ip['var'] > 8]]
+    else:
+        vlines = []
 
     # get difference
     diff_radar_hatpro = radar_lwp
@@ -63,8 +65,6 @@ for begin_dt in dates:
     r_pdata['dt'], r_pdata['var'] = trans._masked_jumps(r_pdata)
     diff_pdata = trans._copy_data(diff_radar_hatpro)
     diff_pdata['dt'], diff_pdata['var'] = trans._masked_jumps(diff_pdata)
-    # get vertical lines at flagged locations
-    vlines = [h.ts_to_dt(t) for t in h_pdata['ts'][hatpro_flag_ip['var'] > 8]]
 
     # plot title
     title = f"Liquid Water Path Comparison LIMRAD94 vs. HATPRO EUREC4A {begin_dt:%Y-%m-%d}"
@@ -75,22 +75,24 @@ for begin_dt in dates:
                      markersize=1, alpha=h_pdata['alpha'], label="LWP HATPRO")
     ax1.plot(matplotlib.dates.date2num(r_pdata['dt'][:]), r_pdata['var'][:], '.',
              markersize=1, alpha=r_pdata['alpha'], label="LWP LIMRAD94")
-    # check if a flag is available for this day
+    # check if positions for vertical lines are available
     if len(vlines) > 0:
         for x in matplotlib.dates.date2num(vlines[:-1]):
-            ax1.axvline(x, alpha=0.5, color='red')
+            ax1.axvline(x, alpha=0.1, color='red')
         # add the last line with label to add to legend
-        ax1.axvline(vlines[-1], alpha=0.5, color='red', label='HATPRO flag')
-        ax1.legend()
+        ax1.axvline(vlines[-1], alpha=0.1, color='red', label='HATPRO flag')
+
+    ax1.legend(bbox_to_anchor=(1.01, 1), loc='upper left')
     ax1, _ = trans._format_axis(fig, ax1, line1, h_pdata)
     ax1.grid()
     ax1.set_xlabel("")
     line2 = ax2.plot(matplotlib.dates.date2num(diff_pdata['dt'][:]), diff_pdata['var'][:], 'g',
                      label='LIMRAD94 - HATPRO')
-    ax2.legend()
+    ax2.legend(bbox_to_anchor=(1.01, 0.7))
     ax2, _ = trans._format_axis(fig, ax2, line2, diff_pdata)
     ax2.grid()
     fig.suptitle(title, size=16)
+    fig.tight_layout()
     figname = f"RV-Meteor_LWP-comp_LIMRAD94-HATPRO_{begin_dt:%Y%m%d}.png"
     plt.savefig(f"../plots/eurec4a_comp_LWP/{figname}")
     plt.close()
