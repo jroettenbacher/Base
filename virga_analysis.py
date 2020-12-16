be0949320ecde586a5e2a2ed58d4d2bf4bb1e87f
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-12-16 11:06:14 +0100

    Create virga_analysis.py
    script to analye virga during eurec4a, on hold until Johannes answers regarding virga cloud sniffer

---
+++ virga_analysis.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""script to analyse virgae during eurec4a
+Idea: compare height of first radar echo and cloud base height detected by ceilometer
+Result:
+    - depth of virga
+    - maximum Ze in virga
+    - dataset
+"""
