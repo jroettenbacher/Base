8e0e2013341d1c8af16dc21dfc39b7cb89310617
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2020-12-21 21:45:11 +0100

    Update loop_make_virga_mask.sh

--- loop_make_virga_mask.sh
+++ loop_make_virga_mask.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 today=$(date +%Y%m%d)
-home="/projekt1/remsens/work/jroettenbacher/Base"
-cd ${home}
+home=/projekt1/remsens/work/jroettenbacher/Base
+cd $home
 for date in {117..126}; do
 	echo "##################################################"
 	echo "#     STARTING DATE 20200${date}                 #"
 	echo "##################################################"
-	/home/jroettenbacher/.conda/envs/jr_base/bin/python3 ${home}/make_virga_mask.py.py date=20200${date} 2>&1 | tee ${home}/log/${today}make_virga_mask.py_200${date}.log
+	/home/jroettenbacher/.conda/envs/jr_base/bin/python3 "$home"/make_virga_mask.py.py date=20200${date} 2>&1 | tee "$home"/log/${today}make_virga_mask.py_200${date}.log
 	echo "Done with 20200${date}"
 done
 
