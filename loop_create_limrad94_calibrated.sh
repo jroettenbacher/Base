22bc4ddf1af85a1bf0f2209ee88b45e2d5a78af2
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-01-29 14:06:19 +0100

    Update loop_create_limrad94_calibrated.sh
    added version keyword

--- loop_create_limrad94_calibrated.sh
+++ loop_create_limrad94_calibrated.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env ksh
 
 home=/projekt1/remsens/work/jroettenbacher/Base
+python=/home/jroettenbacher/.conda/envs/jr_base/bin/python3
 cd ${home}
-version="ca"
+version="jr"
 path="/projekt2/remsens/data_new/site-campaign/rv_meteor-eurec4a/instruments/LIMRAD94/cloudnet_input_heave_cor_"${version}
 for date in {117..126}; do
 	echo "##################################################"
 	echo "#     STARTING DATE 20200${date}                 #"
 	echo "##################################################"
-	/home/jroettenbacher/.conda/envs/jr_base/bin/python3 ${home}/create_limrad94_calibrated_eurec4a.py date=20200${date} path=${path} 2>&1 | tee ${home}/log/200${date}create_limrad94_calibrated_eurec4a_${version}.log
+	${python} ${home}/create_limrad94_calibrated_eurec4a.py date=20200${date} path=${path} heave_corr_version=${version} 2>&1 | tee ${home}/log/200${date}create_limrad94_calibrated_eurec4a_${version}.log
 	echo "Done with 20200${date}"
 done
 
 echo "##################################################"
 echo "#     STARTING DATE 20200128                 #"
 echo "##################################################"
-/home/jroettenbacher/.conda/envs/jr_base/bin/python3 ${home}/create_limrad94_calibrated_eurec4a.py date=20200128 path=${path} 2>&1 | tee ${home}/log/200128_create_limrad94_calibrated_eurec4a_${version}.log
+${python} ${home}/create_limrad94_calibrated_eurec4a.py date=20200128 path=${path} heave_corr_version=${version} 2>&1 | tee ${home}/log/200128_create_limrad94_calibrated_eurec4a_${version}.log
 echo "Done with 20200128"
 
 for date in {201..229}; do
 	echo "##################################################"
 	echo "#     STARTING DATE 20200${date}                 #"
 	echo "##################################################"
-	/home/jroettenbacher/.conda/envs/jr_base/bin/python3 ${home}/create_limrad94_calibrated_eurec4a.py date=20200${date} path=${path} 2>&1 | tee ${home}/log/200${date}_create_limrad94_calibrated_eurec4a_${version}.log
+	${python} ${home}/create_limrad94_calibrated_eurec4a.py date=20200${date} path=${path} heave_corr_version=${version} 2>&1 | tee ${home}/log/200${date}_create_limrad94_calibrated_eurec4a_${version}.log
 	echo "Done with 20200${date}"
 done
