7cc038a6b05c0838f47cdba7ff0c38f9cdece7dc
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-01-29 13:59:32 +0100

    Update loop_create_limrad94_calibrated.sh
    automated logfile to include version used

--- loop_create_limrad94_calibrated.sh
+++ loop_create_limrad94_calibrated.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env ksh
 
-cd /projekt1/remsens/work/jroettenbacher/Base
-path='/projekt2/remsens/data_new/site-campaign/rv_meteor-eurec4a/instruments/LIMRAD94/cloudnet_input_heave_cor_ca'
+home=/projekt1/remsens/work/jroettenbacher/Base
+cd ${home}
+version="ca"
+path="/projekt2/remsens/data_new/site-campaign/rv_meteor-eurec4a/instruments/LIMRAD94/cloudnet_input_heave_cor_"${version}
 for date in {117..126}; do
 	echo "##################################################"
 	echo "#     STARTING DATE 20200${date}                 #"
 	echo "##################################################"
-	/home/jroettenbacher/.conda/envs/jr_base/bin/python3 /projekt1/remsens/work/jroettenbacher/Base/create_limrad94_calibrated_eurec4a.py date=20200${date} path=${path} 2>&1 | tee /projekt1/remsens/work/jroettenbacher/Base/log/200${date}create_limrad94_calibrated_eurec4a_jr.log
+	/home/jroettenbacher/.conda/envs/jr_base/bin/python3 ${home}/create_limrad94_calibrated_eurec4a.py date=20200${date} path=${path} 2>&1 | tee ${home}/log/200${date}create_limrad94_calibrated_eurec4a_${version}.log
 	echo "Done with 20200${date}"
 done
 
 echo "##################################################"
 echo "#     STARTING DATE 20200128                 #"
 echo "##################################################"
-/home/jroettenbacher/.conda/envs/jr_base/bin/python3 /projekt1/remsens/work/jroettenbacher/Base/create_limrad94_calibrated_eurec4a.py date=20200128 path=${path} 2>&1 | tee /projekt1/remsens/work/jroettenbacher/Base/log/200128_create_limrad94_calibrated_eurec4a_jr.log
+/home/jroettenbacher/.conda/envs/jr_base/bin/python3 ${home}/create_limrad94_calibrated_eurec4a.py date=20200128 path=${path} 2>&1 | tee ${home}/log/200128_create_limrad94_calibrated_eurec4a_${version}.log
 echo "Done with 20200128"
 
 for date in {201..229}; do
 	echo "##################################################"
 	echo "#     STARTING DATE 20200${date}                 #"
 	echo "##################################################"
-	/home/jroettenbacher/.conda/envs/jr_base/bin/python3 /projekt1/remsens/work/jroettenbacher/Base/create_limrad94_calibrated_eurec4a.py date=20200${date} path=${path} 2>&1 | tee /projekt1/remsens/work/jroettenbacher/Base/log/200${date}_create_limrad94_calibrated_eurec4a_jr.log
+	/home/jroettenbacher/.conda/envs/jr_base/bin/python3 ${home}/create_limrad94_calibrated_eurec4a.py date=20200${date} path=${path} 2>&1 | tee ${home}/log/200${date}_create_limrad94_calibrated_eurec4a_${version}.log
 	echo "Done with 20200${date}"
 done
