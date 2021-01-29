95397a372606f9b901cdd77fc27620d434933ab6
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-01-29 10:27:40 +0100

    added version keyword and plot path

--- heave_corr_fft_check.py
+++ heave_corr_fft_check.py
@@ -32,6 +32,8 @@ else:
     begin_dt = datetime.datetime.strptime(date + ' 00:00:05', '%Y%m%d %H:%M:%S')
     end_dt = datetime.datetime.strptime(date + ' 23:59:55', '%Y%m%d %H:%M:%S')
 
+version = kwargs['version'] if 'version' in kwargs else 'jr'
+plot_path = f'/projekt2/remsens/data_new/site-campaign/rv_meteor-eurec4a/instruments/LIMRAD94/cloudnet_input_heave_cor_{version}/fft_check'
 range_ = [0, 'max']
 TIME_SPAN_ = [begin_dt, end_dt]
 
@@ -42,13 +44,14 @@ mdv = larda.read("LIMRAD94", "VEL", TIME_SPAN_, range_)
 mdv['var'][mdv['mask']] = np.nan
 # load additional variables
 mdv.update({var: larda.read("LIMRAD94", var, TIME_SPAN_, range_) for var in ['C1Range', 'C2Range', 'C3Range']})
-mdv_cor = larda.read("LIMRAD94_cni_hc_jr", "Vel", TIME_SPAN_, range_)
+mdv_cor = larda.read(f"LIMRAD94_cni_hc_{version}", "Vel", TIME_SPAN_, range_)
 mdv_cor['var'][mdv_cor['mask']] = np.nan
-mdv_cor_roll = larda.read("LIMRAD94_cni_hc_jr", "Vel_roll", TIME_SPAN_, range_)
+mdv_cor_roll = larda.read(f"LIMRAD94_cni_hc_{version}", "Vel_roll", TIME_SPAN_, range_)
 mdv_cor_roll['var'][mdv_cor_roll['mask']] = np.nan
 rg_borders = jr.get_range_bin_borders(3, mdv)
 rg_borders_id = rg_borders - np.array([0, 1, 1, 1])
 chirp_ts = jr.calc_chirp_timestamps(mdv['ts'], begin_dt, version='start')
 chirp_ts_shifted, _ = jr.calc_shifted_chirp_timestamps(mdv['ts'], mdv['var'], chirp_ts, rg_borders_id, 400, Cs_w_radar)
 
-jr.plot_fft_spectra(mdv['var'], chirp_ts, mdv_cor['var'], chirp_ts_shifted, mdv_cor_roll['var'], 3, rg_borders_id, 400, seapath)
+jr.plot_fft_spectra(mdv['var'], chirp_ts, mdv_cor['var'], chirp_ts_shifted, mdv_cor_roll['var'], 3, rg_borders_id, 400,
+                    seapath, pathFig=plot_path)
