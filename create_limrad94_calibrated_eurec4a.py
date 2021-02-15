c07a9341a8d5c56f1f07046b9b14247271509c45
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-02-15 11:09:11 +0100

    changed log level

--- create_limrad94_calibrated_eurec4a.py
+++ create_limrad94_calibrated_eurec4a.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     start_time = time.time()
 
     log = logging.getLogger('pyLARDA')
-    log.setLevel(logging.DEBUG)
+    log.setLevel(logging.INFO)
     log.addHandler(logging.StreamHandler())
 
     larda = pyLARDA.LARDA().connect('eurec4a')
