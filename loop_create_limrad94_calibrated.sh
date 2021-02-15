a3a5e24249cca15f8bb022fc2d24d9009908b9ba
Johannes Röttenbacher <johannes.roettenbacher@web.de>
2021-02-15 14:23:50 +0100

    adjusted loop for aeris upload

--- loop_create_limrad94_calibrated.sh
+++ loop_create_limrad94_calibrated.sh
@@ -9,20 +9,20 @@ for date in {117..126}; do
 	echo "##################################################"
 	echo "#     STARTING DATE 20200${date}                 #"
 	echo "##################################################"
-	${python} ${home}/create_limrad94_calibrated_eurec4a.py date=20200${date} path=${path} heave_corr_version=${version} 2>&1 | tee ${home}/log/200${date}create_limrad94_calibrated_eurec4a_${version}.log
+	${python} ${home}/create_limrad94_calibrated_eurec4a.py date=20200${date} path=${path} heave_corr_version=${version} for_aeris=True 2>&1 | tee ${home}/log/200${date}create_limrad94_calibrated_eurec4a_${version}.log
 	echo "Done with 20200${date}"
 done
 
 echo "##################################################"
 echo "#     STARTING DATE 20200128                 #"
 echo "##################################################"
-${python} ${home}/create_limrad94_calibrated_eurec4a.py date=20200128 path=${path} heave_corr_version=${version} 2>&1 | tee ${home}/log/200128_create_limrad94_calibrated_eurec4a_${version}.log
+${python} ${home}/create_limrad94_calibrated_eurec4a.py date=20200128 path=${path} heave_corr_version=${version} for_aeris=True 2>&1 | tee ${home}/log/200128_create_limrad94_calibrated_eurec4a_${version}.log
 echo "Done with 20200128"
 
 for date in {201..229}; do
 	echo "##################################################"
 	echo "#     STARTING DATE 20200${date}                 #"
 	echo "##################################################"
-	${python} ${home}/create_limrad94_calibrated_eurec4a.py date=20200${date} path=${path} heave_corr_version=${version} 2>&1 | tee ${home}/log/200${date}_create_limrad94_calibrated_eurec4a_${version}.log
+	${python} ${home}/create_limrad94_calibrated_eurec4a.py date=20200${date} path=${path} heave_corr_version=${version} for_aeris=True 2>&1 | tee ${home}/log/200${date}_create_limrad94_calibrated_eurec4a_${version}.log
 	echo "Done with 20200${date}"
 done
